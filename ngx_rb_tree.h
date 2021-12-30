de58a0d5443fa42b83268f4b63a8128825c4c555
million-2002 <76898099+million-2002@users.noreply.github.com>
2021-12-30 18:49:27 +0800

    Create ngx_rb_tree.h

---
+++ ngx_rb_tree.h
@@ -0,0 +1,59 @@
+#include <stdint.h>
+
+typedef struct ngx_rbtree_node_s  ngx_rbtree_node_t;
+typedef uintptr_t ngx_rbtree_key_t;
+
+struct ngx_rbtree_node_s {
+    ngx_rbtree_key_t        key;
+    ngx_rbtree_node_t     *left;
+    ngx_rbtree_node_t     *right;
+    ngx_rbtree_node_t     *parent;
+    char                 color;
+    char                 data;
+};
+
+typedef struct ngx_rbtree_s  ngx_rbtree_t;
+
+typedef void (*ngx_rbtree_insert_pt) (ngx_rbtree_node_t *root,
+    ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel);
+
+struct ngx_rbtree_s {
+    ngx_rbtree_node_t     *root;
+    ngx_rbtree_node_t     *sentinel;
+    ngx_rbtree_insert_pt   insert;
+ };
+
+#define ngx_rbtree_init(tree, s, i)                                           \
+    ngx_rbtree_sentinel_init(s);                                              \
+    (tree)->root = s;                                                         \
+    (tree)->sentinel = s;                                                     \
+    (tree)->insert = i
+
+
+void ngx_rbtree_insert(ngx_rbtree_t *tree, ngx_rbtree_node_t *node);
+void ngx_rbtree_delete(ngx_rbtree_t *tree, ngx_rbtree_node_t *node);
+
+
+#define ngx_rbt_red(node)               ((node)->color = 1)
+#define ngx_rbt_black(node)             ((node)->color = 0)
+#define ngx_rbt_is_red(node)            ((node)->color)
+#define ngx_rbt_is_black(node)          (!ngx_rbt_is_red(node))
+#define ngx_rbt_copy_color(n1, n2)      (n1->color = n2->color)
+
+
+#define ngx_node_data(q, type, member)                                         \
+((type *)((char *)(q)-(unsigned long)(&((type *)0)->member)))
+// 二级指针
+
+/* a sentinel must be black */
+
+#define ngx_rbtree_sentinel_init(node)    ngx_rbt_black(node);              
+
+ngx_rbtree_node_t *ngx_rbtree_min(ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel)
+{
+    while (node->left != sentinel) {
+        node = node->left;
+    }
+
+    return node;
+}
